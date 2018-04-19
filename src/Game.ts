0c68ab3f67973179341da3b33cd22c0cc29ca7e1
Pavel Shakhov <ab.pongo@gmail.com>
2018-04-19 22:46:44 +0300

    Для Game добавлен тест Four Throws No Mark

--- src/Game.ts
+++ src/Game.ts
@@ -1,11 +1,30 @@
 export default class Game {
   private itsScore: number = 0;
+  private itsThrows: number[] = Array(21);
+  private itsCurrentThrow: number = 0;
 
   score(): number {
     return this.itsScore;
   }
 
   add(pins: number) {
+    this.itsThrows[this.itsCurrentThrow] = pins;
+    this.itsCurrentThrow += 1;
     this.itsScore += pins;
+  }
+
+  scoreForFrame(frame: number): number {
+    let score: number = 0;
+    let frameCopy = frame;
+
+    for (
+      let ball = 0;
+      frameCopy > 0 && ball < this.itsCurrentThrow;
+      ball += 2, frameCopy -= 1
+    ) {
+      score += this.itsThrows[ball] + this.itsThrows[ball + 1];
+    }
+
+    return score;
   }
 }
--- src/tests/Game.test.ts
+++ src/tests/Game.test.ts
@@ -16,4 +16,15 @@ export class GameTest {
     g.add(4);
     should(g.score()).equal(9);
   }
+
+  'Four Throws No Mark'() {
+    const g = new Game();
+    g.add(5);
+    g.add(4);
+    g.add(7);
+    g.add(2);
+    should(g.score()).equal(18);
+    should(g.scoreForFrame(1)).equal(9);
+    should(g.scoreForFrame(2)).equal(18);
+  }
 }
